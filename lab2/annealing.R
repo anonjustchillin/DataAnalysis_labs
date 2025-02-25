188cb63603ba73d64a2109ed134b50647f6244a2
Tina Topka <anon.justch1ll1n@gmail.com>
2025-02-25 23:10:21 +0200

    added comments

--- lab2/annealing.R
+++ lab2/annealing.R
@@ -2,13 +2,18 @@ library(optimization)
 
 
 func.annealing <- function(x, y, func){
+  # межі
   lower <- c(x[1], y[1])
   upper <- c(x[length(x)], y[length(y)])
-
+  
+  # виклик функції optim_sa,
+  # яка приймає цільову функцію,
+  # стартові значення, межі та кількість ітерацій
   func.sa <- optim_sa(fun=func,
                       start=c(0,0),
                       lower=lower,
-                      upper=upper)
+                      upper=upper,
+                      control=list(nlimit=5000))
   print(func.sa)
 }
   
--- lab2/stochastic.R
+++ lab2/stochastic.R
@@ -1,19 +1,25 @@
 func.stochastic <- function(x, y, func){ 
+  # межі
   lower <- c(x[1], y[1])
   upper <- c(x[length(x)], y[length(y)])
-  e <- runif(1)
+  # максимальна кількість ітерацій
   N <- 5000
   
+  # значення для збереження результатів
   z.min <- NULL
   par.res <- list(0, 0)
+  # цикл на N ітерацій
   for (i in 1:N){
-    x.rand <- runif(1:N, lower[1], upper[1])
-    y.rand <- runif(1:N, lower[2], upper[2])
-
+    # беремо рандомні x та y
+    x.rand <- runif(1, lower[1], upper[1])
+    y.rand <- runif(1, lower[2], upper[2])
+    # обчислюємо функцію, де x=x.rand, y=y.rand
     z.curr <- func(c(x.rand, y.rand))
     
-    if (i == 1) z.min <- z.curr
+    if (i == 1) z.min <- z.curr # збереження значення з першого обчислення
     else if (z.min > z.curr){
+      # якщо збережене значення більше за поточне,
+      # зберігаємо нові значення
       z.min <- z.curr
       par.res <- replace(par.res, 1, x.rand)
       par.res <- replace(par.res, 2, y.rand)
